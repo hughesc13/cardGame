e944b55da9ecbdc753e4952edc72731052dea604
hughesc13 <60516530+hughesc13@users.noreply.github.com>
2022-12-13 18:03:04 -0500

    removed devtools warning

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ import Deck from "./deck.js"
 console.log("helloworld")
 
 const deck = new Deck()
-deck.shuffle()
+//deck.freshDeck()
+let array = ["h", "e", "l", "l"]
+console.log(array)
 console.log(deck.cards)
 
 //stopped vid at 13:42
